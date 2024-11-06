cb8720b87088b6fd7ff1621999495463c9ee2c18
Nicolas Guerrero <nicolasguerrerop592@gmail.com>
2024-11-06 08:26:18 -0400

    updated personaje.js , mago.js

--- mago.js
+++ mago.js
@@ -1,30 +1,38 @@
+const { Character } = require('./personaje');
+
 class Mage extends Character {
-  constructor(name){
-    super(name, 50, 15, 5);
+  constructor(name) {
+    super(name, 50, 20, 5, 8);
     this.mana = 100;
     this.spells = [
       { name: "Fireball", damage: 20, costMana: 15 },
-      { name: "Ray", daño: 25, costMana: 20 },
+      { name: "Ray", damage: 25, costMana: 20 },
       { name: "Ice Storm", damage: 15, costMana: 10 },
       { name: "Arcane Barrage", damage: 30, costMana: 25 }
   ];
-
-  function ThrowSpell(objetive) {
-    const randomSpell = this.spells[Math.floor(Math.random() * this.spells.length)];
-
-    // Comprobar si tiene suficiente mana
-    if (this.mana >= randomSpell.costMana) {
-      this.mana -= randomSpell.costMana;
-      console.log(`${this.nombre} lanza ${randomSpell.name} a ${objetive.name}, causando ${randomSpell.damage} de daño.`);
-      objetive.reciveDamage(randomSpell.damage);
-      console.log(`Mana restante: ${this.mana}`);
-  } else {
-      console.log(`${this.name} no tiene suficiente mana para lanzar ${randomSpell.name}. Mana restante: ${this.mana}`);
-  }
-  }
-
-  }
-
 }
 
-export { Mage };
+  attack(target) {
+    if (this.mana > 0) {
+      this.throwSpell(target);
+    } else {
+      console.log(`${this.name} no tiene mana y ataca físicamente`);
+      super.attack(target);
+    }
+  }
+
+  throwSpell(target) {
+    const randomSpell = this.spells[Math.floor(Math.random() * this.spells.length)];
+    if (this.mana >= randomSpell.costMana) {
+      this.mana -= randomSpell.costMana;
+      console.log(`${this.name} lanza ${randomSpell.name} a ${target.name}, causando ${randomSpell.damage} de daño.`);
+      target.receiveDamage(randomSpell.damage);
+      console.log(`Mana restante: ${this.mana}`);
+    } else {
+      console.log(`${this.name} no tiene suficiente mana y ataca físicamente`);
+      super.attack(target);
+    }
+  }
+}
+
+module.exports = { Mage };
--- personaje.js
+++ personaje.js
@@ -1,31 +1,32 @@
 class Character {
-  constructor(name,life, attack, defense, speed) {
+  constructor(name, life, attackPower, defense, speed) {
     this.name = name;
     this.life = life;
-    this.attack = attack;
+    this.attackPower = attackPower;
     this.defense = defense;
     this.speed = speed;
     this.experience = 0;
     this.level = 1;
-    function attack(objetive) {
-      const damage = this.attack - objetive.defense;
-      console.log(`${this.name} ataca a ${objetive.name} causando ${damage} puntos de daño.`);
-      objetive.reciveDamage(damage);
-    }
-
-    function reciveDamage(damage) {
-      this.life -= damage;
-      console.log(`${this.name} recibe ${damage} puntos de daño.`);
   }
 
-  function levelUp() {
+  attack(target) {
+    const damage = Math.max(this.attackPower - target.defense, 1);
+    console.log(`${this.name} ataca a ${target.name} causando ${damage} puntos de daño.`);
+    target.receiveDamage(damage);
+  }
+
+  receiveDamage(damage) {
+    this.life -= damage;
+    console.log(`${this.name} recibe ${damage} puntos de daño. Vida restante: ${this.life}`);
+  }
+
+  levelUp() {
     this.level += 1;
-    this.attack += 1;
+    this.attackPower += 1;
     this.defense += 1;
     this.speed += 1;
     this.experience = 0;
   }
 }
-}
 
-export { Character };
+module.exports = { Character };
