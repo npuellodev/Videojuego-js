ea0550c50210a5c6d9cc588a551fdb6646d5ade0
Nicolas Guerrero <nicolasguerrerop592@gmail.com>
2024-11-06 22:03:04 -0400

    se actualizaron los nombres y la salida del mana

--- mago.js
+++ mago.js
@@ -5,10 +5,10 @@ class Mage extends Character {
     super(name, 50, 20, 5, 8);
     this.mana = 100;
     this.spells = [
-      { name: "Fireball", damage: 20, costMana: 15 },
-      { name: "Ray", damage: 25, costMana: 20 },
-      { name: "Ice Storm", damage: 15, costMana: 10 },
-      { name: "Arcane Barrage", damage: 30, costMana: 25 }
+      { name: "Bola de Fuego", damage: 20, costMana: 15 },
+      { name: "Rayo", damage: 25, costMana: 20 },
+      { name: "Tormenta de hielo", damage: 15, costMana: 10 },
+      { name: "Lanza de fuego", damage: 30, costMana: 25 }
   ];
 }
 
@@ -27,7 +27,7 @@ class Mage extends Character {
       this.mana -= randomSpell.costMana;
       console.log(`${this.name} lanza ${randomSpell.name} a ${target.name}, causando ${randomSpell.damage} de daño.`);
       target.receiveDamage(randomSpell.damage);
-      console.log(`Mana restante: ${this.mana}`);
+      console.log(`${this.name} tiene Mana: ${this.mana}`);
     } else {
       console.log(`${this.name} no tiene suficiente mana y ataca físicamente`);
       super.attack(target);
