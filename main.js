08bfddeb9f4d2c0f5cc06b54e8c2c05eaa53ec0c
Nicolas Guerrero <nicolasguerrerop592@gmail.com>
2024-11-06 22:02:49 -0400

    se corrigio el error de la salida de 2 campeones

--- main.js
+++ main.js
@@ -6,22 +6,38 @@ const mage = new Mage("Gandalf");
 const warrior = new Warrior("Legolas");
 
 function startbattle(character1, character2) {
-  while (character1.life > 0 && character2.life > 0) {
+  console.log(`¡Iniciando batalla entre ${character1.name} y ${character2.name}!`);
+
+  while (true) {
     if(character1.speed > character2.speed) {
       character1.attack(character2);
-      if(character2.life > 0) {
-        character2.attack(character1);
+      // Verificar si character2 murió
+      if(character2.life <= 0) {
+        console.log(`${character1.name} ha ganado la batalla!`);
+        break;
+      }
+      character2.attack(character1);
+      // Verificar si character1 murió
+      if(character1.life <= 0) {
+        console.log(`${character2.name} ha ganado la batalla!`);
+        break;
       }
     } else {
       character2.attack(character1);
-      if(character1.life > 0) {
-        character1.attack(character2);
+      // Verificar si character1 murió
+      if(character1.life <= 0) {
+        console.log(`${character2.name} ha ganado la batalla!`);
+        break;
+      }
+
+      character1.attack(character2);
+      // Verificar si character2 murió
+      if(character2.life <= 0) {
+        console.log(`${character1.name} ha ganado la batalla!`);
+        break;
       }
     }
   }
-
-  const winner = character1.life > 0 ? character1 : character2;
-  console.log(winner.name + " ha ganado la batalla!");
 }
 
 startbattle(mage,warrior);
